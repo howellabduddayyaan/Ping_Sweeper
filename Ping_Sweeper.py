c7813f080aff34112e9ba6b50ab6aac309062708
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-06-28 20:02:38 +0200

    Feat : List online devices

--- Ping_Sweeper.py
+++ Ping_Sweeper.py
@@ -10,6 +10,8 @@ network = input("Enter network (e.g. 192.168.1): ")
 
 print("\nSweeping network...\n")
 
+# _________________________________________________________________________________________________
+
 devices_found = []
 
 for i in range(1, 255):
@@ -27,3 +29,19 @@ for i in range(1, 255):
 
 print("\nSweep Complete.\n")
 
+# _________________________________________________________________________________________________
+
+if devices_found:
+
+    print("Online Devices")
+    print("--------------")
+
+    for number, ip in enumerate(devices_found, start=1):
+        print(f"{number}. {ip}")
+
+    print(f"\nTotal Devices Found: {len(devices_found)}")
+
+else:
+    print("No online devices were found.")
+    
+# _________________________________________________________________________________________________
