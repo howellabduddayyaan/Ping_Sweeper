77f11229bc08558d4ac0ce172c7f058c241af3bc
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-06-28 19:59:13 +0200

    Feat : Files and user input prompt added

---
+++ Ping_Sweeper.py
@@ -0,0 +1,14 @@
+# ======================
+# === Ping Sweeper =====
+# ======================
+
+import subprocess
+
+print("\n=== Ping Sweeper ===\n")
+
+network = input("Enter network (e.g. 192.168.1): ")
+
+print("\nSweeping network...\n")
+
+devices_found = []
+
